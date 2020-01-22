79798295eb30057307c76a4dc168e764e1c51304
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-22 01:34:55 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introdution_with_language(name, "Ruby")
+  puts "Hi, my name is #{name} and I'm learning to program in #{language}."
+end
