16fba1c4d48adaab28d30fad5af1decbfb2af89b
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-24 21:27:05 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,1 +1,9 @@
 lib/ introduction.rb
+def introduction (name)
+  puts "Hi, my name is #{name}."
+end
+
+def# introduction_with_language method
+phrase = 'name', 'language'
+puts "Hi, my name is #{phrase} and I am learning to program in #{language}."
+end
